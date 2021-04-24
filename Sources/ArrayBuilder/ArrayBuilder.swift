fc06af7372b00661f0f34578551c8abe96015403
Julian Dunskus <julian.dunskus@gmail.com>
2021-04-24 19:22:41 +0200

    make builder methods public
    (oops)

--- Sources/ArrayBuilder/ArrayBuilder.swift
+++ Sources/ArrayBuilder/ArrayBuilder.swift
@@ -6,35 +6,35 @@ public extension Array {
 
 @_functionBuilder
 public enum ArrayBuilder<Element> {
-	static func buildExpression(_ element: Element) -> [Element] {
+	public static func buildExpression(_ element: Element) -> [Element] {
 		[element]
 	}
-	static func buildExpression(_ element: Element?) -> [Element] {
+	public static func buildExpression(_ element: Element?) -> [Element] {
 		element.map { [$0] } ?? []
 	}
-	static func buildExpression(_ elements: [Element]) -> [Element] {
+	public static func buildExpression(_ elements: [Element]) -> [Element] {
 		elements
 	}
 	
-	static func buildBlock(_ content: [Element]) -> [Element] {
+	public static func buildBlock(_ content: [Element]) -> [Element] {
 		content
 	}
-	static func buildBlock(_ contents: [Element]...) -> [Element] {
+	public static func buildBlock(_ contents: [Element]...) -> [Element] {
 		Array(contents.joined())
 	}
 	
-	static func buildOptional(_ content: [Element]?) -> [Element] {
+	public static func buildOptional(_ content: [Element]?) -> [Element] {
 		content ?? []
 	}
 	
-	static func buildEither(first: [Element]) -> [Element] {
+	public static func buildEither(first: [Element]) -> [Element] {
 		first
 	}
-	static func buildEither(second: [Element]) -> [Element] {
+	public static func buildEither(second: [Element]) -> [Element] {
 		second
 	}
 	
-	static func buildArray(_ contents: [[Element]]) -> [Element] {
+	public static func buildArray(_ contents: [[Element]]) -> [Element] {
 		Array(contents.joined())
 	}
 }
